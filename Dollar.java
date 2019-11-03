7cf339cfea486cf65f2b92b23ff783a4f52a9b4e
Gabriel P <gabrielpires@dcc.ufmg.br>
2019-11-03 14:27:26 -0300

    Commit 3

--- Dollar.java
+++ Dollar.java
@@ -1,9 +1,10 @@
 class Dollar extends Money{
 
-    Dollar(int amount) {
-        this.amount= amount;
+    Dollar(int amount){
+        this.amount = amount;
     }
-    void times(int multiplier) {
-        amount= amount * multiplier;
+
+    Money times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
-}	
+}
--- Franc.java
+++ Franc.java
@@ -1,9 +1,10 @@
-class Franc extends Money {
+class Franc extends Money{
 
-    Franc(int amount) {
-        this.amount= amount;
+    Franc(int amount){
+        this.amount = amount;
     }
-    Franc times(int multiplier)  {
+
+    Money times(int multiplier)  {
         return new Franc(amount * multiplier);
     }
 }
